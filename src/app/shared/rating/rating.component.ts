d8e9b348f0d35b08b1c3a7d899c20ff3004bad95
Fabien Laurette <fabien.laurette@gmail.com>
2021-03-10 22:30:34 +0100

    CSS fiche detail + forms

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -8,15 +8,18 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RatingComponent implements OnInit {
 
   @Input() rating: number;
+  @Input() votes: number;
   
   //Array pour utilisation du ngFor (on insère autant d'éléments que de points, pour boucler autant de fois)
   stars:Array<number>;
+  greyStars:Array<number>;
 
   constructor() { }
 
   ngOnInit(): void {
- //   console.log(this.rating);
     let roundedScore = Math.round(this.rating);
+    let reste = 5 - roundedScore;
     this.stars = new Array(roundedScore).fill(1);
+    this.greyStars = new Array(reste).fill(1);
   }
 }
