1fba971a7ac4fc293602709bebc7b1a01e79c7ec
Fabien Laurette <fabien.laurette@gmail.com>
2021-03-11 10:01:52 +0100

    correction bug logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RegisterFormComponent } from './register-form/register-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'logout', component: HomeComponent },
   { path: 'mylist', component: MediaListComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
