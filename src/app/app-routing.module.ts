e9fa75e4e55b5481907505389d5ceb11e27b90af
Fabien Laurette <fabien.laurette@gmail.com>
2021-03-11 22:48:56 +0100

    ajout page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { HomeComponent } from './home/home.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { MediaListComponent } from './media-list/media-list.component';
@@ -12,7 +13,9 @@ const routes: Routes = [
   { path: 'mylist', component: MediaListComponent },
   { path: 'login', component: LoginFormComponent },
   { path: 'register', component: RegisterFormComponent },
-  { path: 'detail/:id/:type', component: DetailComponent }
+  { path: 'detail/:id/:type', component: DetailComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
